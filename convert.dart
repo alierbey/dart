5fa481cd4d4964ddaa4657e67a4c40133234fd33
ali erbey <alierbey@alis-MacBook-Pro.local>
2020-11-24 15:34:23 +0300

    conver2

--- convert.dart
+++ convert.dart
@@ -1,5 +1,25 @@
 void main() {
+  // Int -> String
   int a = 12;
   String b = a.toString();
   print(b);
+  print(b.runtimeType);
+
+  // String -> Int
+  String c = "12";
+  int d = int.parse(c);
+  print(d);
+  print(d.runtimeType);
+
+  // Double -> String
+  double e = 10.213456;
+  String f = e.toStringAsFixed(3);
+  print(f);
+  print(f.runtimeType);
+
+  // String to Double
+  String g = "12";
+  double h = double.parse(g);
+  print(h);
+  print(h.runtimeType);
 }
