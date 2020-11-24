6e823e9ad143d16a0dfe020ab0b40202b65140f0
ali erbey <alierbey@alis-MacBook-Pro.local>
2020-11-24 15:10:03 +0300

    first

---
+++ convert.dart
@@ -0,0 +1,4 @@
+void main() {
+  int a = 12;
+  print(Type(a));
+}
---
+++ variables.dart
@@ -0,0 +1,17 @@
+void main() {
+  var k = "ali";
+  int a = 10;
+  double b = 0.2;
+  bool c = true;
+  String d = "ali erbey";
+  int sayi = 12; // null dur
+  assert(sayi == null, "yokya");
+
+  print(k);
+  print(a);
+  print("a degiskeni = $a");
+  print(b);
+  print(c);
+  print(d);
+  print(sayi);
+}
