3cad927c62429c854d7b421584834fc1ac84e8bb
ali erbey <alierbey@alis-MacBook-Pro.local>
2020-11-24 19:43:44 +0300

    list

---
+++ list.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  // List tanımlama yöntemleri
+  List<int> list = [12, 34, 45];
+  
+  yeniList = []
+
+  print(list);
+  // List in eleman sayısı
+  print(list.length);
+
+  // List'in ilk elemanı
+  print(list[0]);
+
+  // List'in 2 indisli elamanının değerini değiştirme
+  list[2] = 5;
+  print(list);
+
+  
+}
