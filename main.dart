eb3f20685f1c14b1f6d44f31938fb612ca9af3fc
yapraktunc <yaprak.tunc35@yahoo.com>
2022-01-12 20:18:33 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,11 @@
 // 192855037 - İlhami Tuğral
 
-String university = 'Uşak Üniversitesi';
+//String university = 'Uşak Üniversitesi';
+
+String university = 'AÖF Üniversitesi';
 
 void main() {
+ // print("${university}'ne hoşgeldiniz");
   print("${university}'ne hoşgeldiniz");
   print("yeap");
 }
